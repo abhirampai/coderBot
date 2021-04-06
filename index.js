1f74f3090759c687832fa8b342514a5860ccb0ea
Pai026 <abhirampai1999@gmail.com>
2021-04-06 10:23:04 +0530

    prettier formatted

--- index.js
+++ index.js
@@ -3,70 +3,70 @@ const client = new Discord.Client();
 var request = require("request");
 require("dotenv").config();
 
-const prefix = "DM ";
+const prefix = "coder ";
 
 client.on("ready", () => {
-    console.log("DM-BOT is online!");
-  });
-  
-  client.on("message", async (message) => {
-    try {
-      let args = message.content.substring(prefix.length).split(" ");
-      switch (args[0]) {case "run":
-      var input = "";
-      var language, index;
-      if (args[1] == "C" || args[1] == "c") {
-        language = "c";
-        index = "0";
-      } else if (args[1] == "python") {
-        language = "python3";
-        index = "3";
-      }
-      var fileName = message.attachments.array()[0];
-      for (var i = 2; i < args.length; i++) input += args[i] + " ";
+  console.log("coder-BOT is online!");
+});
 
-      request.get(fileName.url, (err, response, body) => {
-        const runRequestBody = {
-          script: body,
-          language: language,
-          stdin: input,
-          versionIndex: index,
-          clientId: process.env.CLIENT_ID,
-          clientSecret: process.env.CLIENT_SECRET,
-        };
-        request
-          .post({
-            url: "https://api.jdoodle.com/execute",
-            json: runRequestBody,
-          })
-          .on("error", (error) => {
-            console.log("request.post error", error);
-            return;
-          })
-          .on("data", (data) => {
-            const parsedData = JSON.parse(data.toString());
-            if (parsedData.error) {
+client.on("message", async (message) => {
+  try {
+    let args = message.content.substring(prefix.length).split(" ");
+    switch (args[0]) {
+      case "run":
+        var input = "";
+        var language, index;
+        if (args[1] == "C" || args[1] == "c") {
+          language = "c";
+          index = "0";
+        } else if (args[1] == "python") {
+          language = "python3";
+          index = "3";
+        }
+        var fileName = message.attachments.array()[0];
+        for (var i = 2; i < args.length; i++) input += args[i] + " ";
+
+        request.get(fileName.url, (err, response, body) => {
+          const runRequestBody = {
+            script: body,
+            language: language,
+            stdin: input,
+            versionIndex: index,
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+          };
+          request
+            .post({
+              url: "https://api.jdoodle.com/execute",
+              json: runRequestBody,
+            })
+            .on("error", (error) => {
+              console.log("request.post error", error);
               return;
-            } else {
-              var output = "";
-              for (var i = 0; i < parsedData.output.length; i++) {
-                if (parsedData.output[i] == "\n") output += "\n";
-                else output += parsedData.output[i];
+            })
+            .on("data", (data) => {
+              const parsedData = JSON.parse(data.toString());
+              if (parsedData.error) {
+                return;
+              } else {
+                var output = "";
+                for (var i = 0; i < parsedData.output.length; i++) {
+                  if (parsedData.output[i] == "\n") output += "\n";
+                  else output += parsedData.output[i];
+                }
+                let outputOfProgram = new Discord.MessageEmbed()
+                  .setAuthor(client.user.username)
+                  .setDescription("Result!!!")
+                  .setColor("33FFB3")
+                  .setDescription(output);
+                message.channel.send(outputOfProgram);
+                return;
               }
-              let outputOfProgram = new Discord.MessageEmbed()
-                .setAuthor(client.user.username)
-                .setDescription("Result!!!")
-                .setColor("33FFB3")
-                .setDescription(output);
-              message.channel.send(outputOfProgram);
-              return;
-            }
-          });
-      });
-      break;
+            });
+        });
+        break;
     }
-}
-catch (Exception) {
+  } catch (Exception) {
     console.log(Exception);
     let Errorbotembed = new Discord.MessageEmbed()
       .setAuthor(client.user.username)
@@ -76,6 +76,5 @@ catch (Exception) {
     message.channel.send(Errorbotembed);
   }
 });
-
 
 client.login(process.env.CLIENT_TOKEN);
